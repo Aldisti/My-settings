7f8cf2dce9d3bf6945f4f73a5818621f62ae89ce
aldisti <alessandrodiste99@gmail.com>
2025-07-13 20:46:04 +0200

    added an automatic install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# run this script to setup everything
+# it creates links to make available the gcln and setjava commands
+
+COMMANDS_DIR="/usr/local/bin"
+
+ln -s $PWD/gcln.sh $COMMANDS_DIR/gcln
+ln -s $PWD/setjava.sh $COMMANDS_DIR/setjava
+
+echo -e "\nsource $PWD/mybashrc.sh\n" >> $HOME/.bashrc
+
+mkdir $HOME/.vim
+ln -s $PWD/vimrc $HOME/.vim/vimrc
+
