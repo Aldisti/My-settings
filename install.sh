145130aaef7a4d6859db2140078611cd8b8ef929
aldisti <alessandrodiste99@gmail.com>
2025-09-21 21:31:37 +0200

    cleaned things

--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@
 
 COMMANDS_DIR="/usr/local/bin"
 ROOT_DIR="$PWD/$(dirname $0)"
+CMDS_DIR="$ROOT_DIR/commands"
+CONFS_DIR="$ROOT_DIR/confs"
 
 main() {
-	sudo ln -fs $ROOT_DIR/gcln $COMMANDS_DIR/gcln
-    sudo ln -fs $ROOT_DIR/setjava $COMMANDS_DIR/setjava
-    sudo ln -fs $ROOT_DIR/gentok $COMMANDS_DIR/gentok
-    sudo ln -fs $ROOT_DIR/proxy $COMMANDS_DIR/proxy
+	sudo ln -fs $CMDS_DIR/gcln $COMMANDS_DIR/gcln
+    sudo ln -fs $CMDS_DIR/setjava $COMMANDS_DIR/setjava
+    sudo ln -fs $CMDS_DIR/gentok $COMMANDS_DIR/gentok
+    sudo ln -fs $CMDS_DIR/proxy $COMMANDS_DIR/proxy
 
-    echo -e "export MY_BASHRC=$ROOT_DIR/bashrc" >> $HOME/.bashrc
-    echo -e "\nsource \$MY_BASHRC\n" >> $HOME/.bashrc
+    echo -e "\nexport MY_BASHRC=$CONFS_DIR/bashrc" >> $HOME/.bashrc
+    echo -e "source \$MY_BASHRC\n" >> $HOME/.bashrc
 
     set_vim
     set_go
@@ -21,21 +23,21 @@ main() {
 
 set_vim() {
     mkdir -p $HOME/.vim
-    ln -s $ROOT_DIR/vimrc $HOME/.vim/vimrc
+    ln -fs $CONFS_DIR/vimrc $HOME/.vim/vimrc
 
     # install vim plugin manager
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    # curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    #     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 }
 
 set_go() {
     set -e
-    cd src/random-emoji
+    cd $ROOT_DIR/src/random-emoji
     go run gen_emojis.go
     go build -o randmj randmj.go
     rm emojis_zipped
     cd - > /dev/null
-    sudo mv src/random-emoji/randmj $COMMANDS_DIR/.
+    sudo mv $ROOT_DIR/src/random-emoji/randmj $COMMANDS_DIR/.
 }
 
 echo -e "COMMANDS_DIR: $COMMANDS_DIR"
